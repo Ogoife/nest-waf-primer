722e38f0580ca7507021b177afcf3e96bad22295
Ogoife <oifesemen@gmail.com>
2021-02-13 17:25:15 +0100

    commit2

--- src/student-registration/student-registration.module.ts
+++ src/student-registration/student-registration.module.ts
@@ -1,4 +1,8 @@
 import { Module } from '@nestjs/common';
+import { UsersModule } from './users/users.module';
+import { StudentsModule } from './students/students.module';
 
-@Module({})
+@Module({
+  imports: [UsersModule, StudentsModule]
+})
 export class StudentRegistrationModule {}
